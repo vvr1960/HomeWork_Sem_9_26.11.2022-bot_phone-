fc820518bff6309637fbd2e090c94ae44639967b
Vladimir Rozenberg <vvr_prim@mail.ru>
2022-12-04 17:11:26 +1000

    Модуль для вывода всего списка абонентов

---
+++ view_phone_book.py
@@ -0,0 +1,23 @@
+import csv
+import sqlite3 as sql
+import pandas as pd
+
+def data_view_csv_pandas():
+    conn = sql.connect('data_numbers.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM phone_data")
+    conn.commit()
+
+    row_csv_arr = []
+    for row in cursor:
+        row_csv = list(row)
+        row_csv_arr.append(row_csv)
+        
+    with open("data_numbers.csv", mode="w", newline = '', encoding='utf-8') as w_file:
+        file_writer = csv.writer(w_file, delimiter = ",", lineterminator="\r")
+        file_writer.writerow(["Фамилия", "Имя", "Отчество", "Номер_телефона"])
+        file_writer.writerows(sorted(row_csv_arr, key=lambda x: x[0]))
+    df = pd.read_csv('data_numbers.csv') 
+    print(df) 
+      
+#data_view_csv_pandas()
