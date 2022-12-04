49b9de4bfe6e210b7450115f4586537b9b75f354
Vladimir Rozenberg <vvr_prim@mail.ru>
2022-12-04 17:09:54 +1000

    Модуль для вывода номера телефона абонента

---
+++ output_phone_book.py
@@ -0,0 +1,22 @@
+import sqlite3 as sql
+
+global conn
+global cursor
+
+conn = sql.connect('data_numbers.db')
+cursor = conn.cursor()
+
+def abonent_output():
+    print('Введите фамилию абонента:\n')
+    surname_output = input('Фамилия: ')
+    print()
+    
+    cursor.execute(f"SELECT phone_number FROM phone_data WHERE (surname) = ('{surname_output}')")
+    result = cursor.fetchall()
+    if result == []:
+        print('Данный абонент отсутствует в справочнике!')
+    else:
+        for row in result:
+            print(f'Номер телефона: {row[0]}')
+
+#abonent_output()
