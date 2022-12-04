51918dc7df9d3c5e0690b5d4dd002a0052495e90
Vladimir Rozenberg <vvr_prim@mail.ru>
2022-12-04 17:02:12 +1000

    Модуль для ввода нового абонента

---
+++ input_phone_book.py
@@ -0,0 +1,21 @@
+import sqlite3 as sql
+
+global conn
+global cursor
+
+conn = sql.connect('data_numbers.db')
+cursor = conn.cursor()
+
+def data_input():
+    print('Введите данные абонента:\n')
+    surname = input('Фамилия: ')
+    name = input('Имя: ')
+    patron = input('Отчество: ')
+    phone_number = input('Номер телефона: ')
+
+    pers_data = surname, name, patron, phone_number
+    data_phone_list = """INSERT INTO phone_data (surname, name, patron, phone_number) values(?, ?, ?, ?);"""
+    cursor.execute(data_phone_list, pers_data)
+    conn.commit()
+
+#data_input()
