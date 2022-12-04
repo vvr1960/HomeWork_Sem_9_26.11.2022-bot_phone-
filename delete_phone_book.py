935d92b3fa16ce8d35c7f2c87503fd12049a7651
Vladimir Rozenberg <vvr_prim@mail.ru>
2022-12-04 17:03:52 +1000

    Модуль для удаления  абонента из справочника

---
+++ delete_phone_book.py
@@ -0,0 +1,20 @@
+import sqlite3 as sql
+
+global conn
+global cursor
+
+conn = sql.connect('data_numbers.db')
+cursor = conn.cursor()
+
+def delete_abonent():
+    password = input('Введите пароль: ')
+
+    if password != '123456':
+       print('Пароль неверный')
+       exit()
+    else:
+        phone_number_delete = input(f'Введите номер телефона абонента, данные о котором удаляются: ')
+        cursor.execute(f"DELETE from phone_data where phone_number = {phone_number_delete}")
+        conn.commit()
+
+#delete_abonent()
